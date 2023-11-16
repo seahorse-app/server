3997d1af2122c97bc49d3686ff919a1156a68e8e
Leo Breuer <contact@cheetahbyte.dev>
2023-11-16 20:28:16 +0100

    feat(models): add user model

---
+++ models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID           uuid.UUID `json:"id"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	BirthDate    string    `json:"birth_date"`
+	Email        string    `json:"email"`
+	PasswordHash string
+	gorm.Model
+}
