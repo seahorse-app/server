cf7d193cdb78143560c2cf9657164f21e45e6190
Leo Breuer <contact@cheetahbyte.dev>
2023-11-21 06:17:45 +0100

    feat(users/create): add usercreate dto
    add user create dto for validation of incoming (json) data.

--- handlers/user.go
+++ handlers/user.go
@@ -9,6 +9,17 @@ type UserHandler struct {
 	DB *gorm.DB
 }
 
+type UserCreate struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
 func (handler *UserHandler) Create(c *gin.Context) {
-	c.String(200, "Hello World!")
+	var userData UserCreate
+	if err := c.ShouldBindJSON(&userData); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	println(userData.Username)
 }
