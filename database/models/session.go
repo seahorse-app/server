ae10cc7a1d6ad09a6a808676932c2d6c6a0480da
Leo Breuer <contact@cheetahbyte.dev>
2023-11-22 07:00:10 +0100

    feat(models): update session and user model
    - add BeforeCreate Hooks to both models (to include uuid4)
    - relate users to sessions

--- database/models/session.go
+++ database/models/session.go
@@ -4,11 +4,16 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Session struct {
-	ID        uuid.UUID `json:"id"`
+	ID        uuid.UUID `json:"id" gorm:"primaryKey;"`
 	UserID    uuid.UUID `json:"user_id"`
-	User      `gorm:"foreignKey:UserID"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+func (session *Session) BeforeCreate(tx *gorm.DB) (err error) {
+	session.ID = uuid.New()
+	return
 }
--- database/models/user.go
+++ database/models/user.go
@@ -3,17 +3,25 @@ package models
 import (
 	"time"
 
+	"gorm.io/gorm"
+
 	"github.com/google/uuid"
 )
 
 type User struct {
-	ID           uuid.UUID `json:"id"`
+	ID           uuid.UUID `json:"id" gorm:"primaryKey;"`
 	FirstName    string    `json:"first_name"`
 	LastName     string    `json:"last_name"`
 	BirthDate    string    `json:"birth_date"`
 	Email        string    `json:"email"`
 	PasswordHash string    `json:"-"`
+	Sessions     []Session `json:"sessions"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 	DeletedAt    time.Time `gorm:"index"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.ID = uuid.New()
+	return
 }
